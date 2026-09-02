e4d2d7b44d8a90d047363f9d5a908408ebe34fb1
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2026-09-02 16:55:18 +0800

    chore: references component from wwwroot

--- MinimalExample/Views/Home/Index.cshtml.js
+++ MinimalExample/Views/Home/Index.cshtml.js
@@ -1,3 +1,10 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    console.info("Hello world")
+﻿import SomeClass from "/js/components/someClass.js"
+
+/**
+ * @typedef {import('../../wwwroot/js/components/someClass').SomeClassState} SomeClassState
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const someClass = new SomeClass("Hello world")
+    console.info(someClass.title)
 })
---
+++ MinimalExample/wwwroot/js/components/someClass.js
@@ -0,0 +1,19 @@
+﻿
+/**
+ * @typedef {object} SomeClassState
+ * @property {string} title
+ */
+
+
+export default class SomeClass {
+    #state = {
+        title: ""
+    }
+    constructor(title) {
+        this.#state.title = title
+    }
+
+    get title() {
+        return this.#state.title
+    }
+}
